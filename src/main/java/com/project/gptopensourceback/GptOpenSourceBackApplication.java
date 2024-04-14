3ecbacc33eb83d69e18bb63d35c52238318c31a7
MinseoKangQ <kms02171@naver.com>
2024-04-14 22:43:21 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/project/gptopensourceback/GptOpenSourceBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.gptopensourceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptOpenSourceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptOpenSourceBackApplication.class, args);
+	}
+
+}
