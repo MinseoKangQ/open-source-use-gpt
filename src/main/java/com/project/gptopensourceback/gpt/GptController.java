a8100ff3a7c0525713e1cd57d42048f23787c969
MinseoKangQ <kms02171@naver.com>
2024-05-27 03:15:35 +0900

    :bug: GET -> POST 수정

--- src/main/java/com/project/gptopensourceback/gpt/GptController.java
+++ src/main/java/com/project/gptopensourceback/gpt/GptController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class GptController {
         return gptService.askGpt(prompt);
     }
 
-    @GetMapping("projectInfo")
+    @PostMapping("projectInfo")
     public ResponseEntity<CustomApiResponse<?>> askGptProjectInfo(@RequestBody ProjectInfoReqDto projectInfo) {
         return gptService.askGptProjectInfo(projectInfo);
     }
